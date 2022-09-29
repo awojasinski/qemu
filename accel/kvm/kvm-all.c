687ffd45ab9e3a7ed223454ba5fac9341d4b68f8
Alex Bennée <alex.bennee@linaro.org>
2022-09-29 12:42:24 +0100

    gdbstub: move breakpoint logic to accel ops
    As HW virtualization requires specific support to handle breakpoints
    lets push out special casing out of the core gdbstub code and into
    AccelOpsClass. This will make it easier to add other accelerator
    support and reduces some of the stub shenanigans.
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Mads Ynddal <mads@ynddal.dk>
    Message-Id: <20220929114231.583801-45-alex.bennee@linaro.org>
    (cherry picked from commit ae7467b1ac49e10c548099e9f9c59af895af2d3f)

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -3231,8 +3231,7 @@ int kvm_update_guest_debug(CPUState *cpu, unsigned long reinject_trap)
     return data.err;
 }
 
-int kvm_insert_breakpoint(CPUState *cpu, target_ulong addr,
-                          target_ulong len, int type)
+int kvm_insert_breakpoint(CPUState *cpu, int type, hwaddr addr, hwaddr len)
 {
     struct kvm_sw_breakpoint *bp;
     int err;
@@ -3270,8 +3269,7 @@ int kvm_insert_breakpoint(CPUState *cpu, target_ulong addr,
     return 0;
 }
 
-int kvm_remove_breakpoint(CPUState *cpu, target_ulong addr,
-                          target_ulong len, int type)
+int kvm_remove_breakpoint(CPUState *cpu, int type, hwaddr addr, hwaddr len)
 {
     struct kvm_sw_breakpoint *bp;
     int err;
@@ -3337,25 +3335,9 @@ void kvm_remove_all_breakpoints(CPUState *cpu)
 
 #else /* !KVM_CAP_SET_GUEST_DEBUG */
 
-int kvm_update_guest_debug(CPUState *cpu, unsigned long reinject_trap)
+static int kvm_update_guest_debug(CPUState *cpu, unsigned long reinject_trap)
 {
     return -EINVAL;
-}
-
-int kvm_insert_breakpoint(CPUState *cpu, target_ulong addr,
-                          target_ulong len, int type)
-{
-    return -EINVAL;
-}
-
-int kvm_remove_breakpoint(CPUState *cpu, target_ulong addr,
-                          target_ulong len, int type)
-{
-    return -EINVAL;
-}
-
-void kvm_remove_all_breakpoints(CPUState *cpu)
-{
 }
 #endif /* !KVM_CAP_SET_GUEST_DEBUG */
 
